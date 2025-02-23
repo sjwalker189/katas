98bb5b58c89d74a902aa46dc0d395bd85b47dc0d
Sam Walker <sam.walker@aviatnet.com>
2025-02-23 13:43:06 +1300

    Improve bubble sort implementation

--- typescript/algorithims/bubble_sort.ts
+++ typescript/algorithims/bubble_sort.ts
@@ -1,18 +1,16 @@
 export function bubble_sort(list: number[]): number[] {
-  let len = list.length - 1;
-
-  do {
-    for (let i = 0; i < len; i++) {
-      const a = list[i];
-      const b = list[i + 1];
-
-      if (a > b) {
-        list[i] = b;
-        list[i + 1] = a;
+  for (let i = 0; i < list.length; i++) {
+    // the highest number will move to the end on each itteration
+    // so we reduce the length we itterate in the inner loop moving
+    // towards the left until all items are sorted
+    for (let j = 0; j < list.length - 1 - i; j++) {
+      // swap positions
+      if (list[j] > list[j + 1]) {
+        const tmp = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = tmp;
       }
     }
-    len--;
-  } while (len !== 1);
-
+  }
   return list;
 }
